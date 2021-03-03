7680bf7e2500d186f534f6ff573d82995dc64cf2
alexandru <boncut.alex10@gmail.com>
2021-03-03 17:50:33 -0600

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
   # your code here
+  array_sqared = []
+  while array.length != array_sqared.length
+    array.each{|n| array_sqared << n**2} 
+  end
 end
+
+
+
+
+
+def square_array(new_array)
+  new_array.collect{|n| n**2 }
+end
+
